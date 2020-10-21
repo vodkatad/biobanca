3ecc6af9cf7ccaa80bd47bc2c35f8012c512cc27
Elena Grassi <grassi.e@gmail.com>
2020-10-21 12:08:27 +0200

    Fixed cetuximab conf and added plots

--- local/src/many_models.R
+++ local/src/many_models.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+library(ggplot2)
 args <- commandArgs(trailingOnly = T)
 x <- args[1]
 y <- args[2]
@@ -30,8 +31,22 @@ lmp <- function(model) {
   return(p)
 }
 
+
+ggplotRegression <- function(fit, name) {
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+          geom_point() +
+          stat_smooth(method = "lm", col = "blue") +
+          labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                             " P =",signif(summary(fit)$coef[2,4], 5)))+
+          theme_bw()+theme(text = element_text(size=15))
+  ggsave(paste0(name, ".svg"))
+}
+
+
+
 compute_lm <- function(xcol, data, ycol) {
     model <- lm(data=data, formula=as.formula(paste0(ycol, "~", xcol)))
+    ggplotRegression(model, xcol)
     sm <- summary(model)
     r2 <- sm$r.squared
     ar2 <- sm$adj.r.squared
@@ -40,8 +55,6 @@ compute_lm <- function(xcol, data, ycol) {
     return(c(pval, r2, ar2,ll))
 }
 
-save.image('forplot.Rdata') # I stop here to troubleshoot the ggplot for the model with a live Rstudio
 res <- t(sapply(xcolumns, compute_lm, mdata, yname))
 colnames(res) <- c('pvalue','R2','adjR2','logLik')
 write.table(data.frame('exp'=rownames(res), res), file=output, sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
-
---
+++ local/src/plot_models_metrics.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+library(reshape)
+library(ggpubr)
+
+args <- commandArgs(trailingOnly = T)
+infile <- args[1]
+outfile <- args[2]
+
+data <- read.table(infile, sep="\t", header=TRUE)
+data$R2 <- NULL
+data$logLik <- NULL
+data$pvalue <- -log10(data$pvalue)
+m <- melt(data)
+m$exp <- as.character(m$exp)
+m$n <- as.numeric(sapply(strsplit(m$exp, "_"), function(x) {x[2]}))
+m$expn <- sapply(strsplit(m$exp, "_"), function(x) {x[1]})
+m <- m[order(m$expn, m$n),]
+
+mp <- m[m$variable =="pvalue",]
+mr <- m[m$variable =="adjR2",]
+
+pplot <- ggplot(data=mp, aes(x=n, y=value, color=expn))+geom_point()+geom_line()+theme_bw()+theme(legend.position="none", text=element_text(size=15))+scale_x_log10()+scale_x_continuous(breaks=c(1250, 5000, 20000))+xlab('N.cells')+ylab("-Log10(pvalue)")+scale_color_manual(values=c('darkgoldenrod','darkgreen'))
+Rplot <- ggplot(data=mr, aes(x=n, y=value, color=expn))+geom_point()+geom_line()+theme_bw()+theme(text=element_text(size=15), legend.position="bottom")+scale_x_log10()+scale_x_continuous(breaks=c(1250, 5000, 20000))+xlab('N.cells')+ylab("adjusted R2")+scale_color_manual(values=c('darkgoldenrod','darkgreen'))+labs(color="Measurement")
+
+ggarrange(pplot, Rplot, labels = c("A", "B"), ncol = 1, nrow = 2)
+ggsave(outfile)
