3aa6683ada63f39d3526c706f6e535b4a32f2767
Elena Grassi <grassi.e@gmail.com>
2021-07-16 18:49:33 +0200

    [shallowseq] added gistic score scatterplot

--- local/src/genes_delta_gistic_overall.R
+++ local/src/genes_delta_gistic_overall.R
@@ -1,12 +1,47 @@
 # Compute delta of gistic scores for genes
 library(pheatmap)
 
+library(ggplot2)
+th <- function() {
+  textSize <- 1.5
+  theme <- theme_bw() +
+    theme(
+      strip.text = element_text(size = rel(textSize)),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      axis.title = element_text(size = rel(1.8)),
+      axis.text.x = element_text(size=rel(1.7)),
+      axis.text.y = element_text(angle = 0,
+                                 size = rel(1.7)),
+      axis.line = element_line(colour = "black"),
+      axis.ticks.x = element_blank(),
+      axis.ticks.length.y.left = unit(3,'mm'),
+      
+      legend.position = "top",
+      legend.justification = "right",
+      #legend.margin = margin(unit(0, "cm")),
+      legend.title = element_text(size = rel(textSize), face = "bold"),
+      legend.text = element_text(size = rel(1.2)),
+      legend.background = element_rect(size=0.5, linetype="solid", color="black"),
+      plot.title = element_text(
+        face = "bold",
+        size = rel(2),
+        hjust = 0.5
+      ),
+      panel.border = element_blank(),
+      plot.caption = element_text(size=rel(1))
+    )
+  theme
+}
+current_theme <- th()
 #pdo_gistic_f <- '/mnt/trcanmed/snaketree/prj/biobanca/local/share/data/shallowseq/gistic/gistic_pdo/all_data_by_genes.txt'
 #pdx_gistic_f <- '/mnt/trcanmed/snaketree/prj/biobanca/local/share/data/shallowseq/gistic/gistic_xeno/all_data_by_genes.txt'
 pdo_gistic_f <- snakemake@input[['pdo']]
 pdx_gistic_f <- snakemake@input[['pdx']]
 delta_f <- snakemake@output[['delta']]
 plot_f <- snakemake@output[['delta_plot']]
+plot2_f <- snakemake@output[['scores_plot']]
+
 
 load_prepare_df <- function(filename) {
   df <- read.table(filename, sep="\t", header=FALSE, stringsAsFactors = FALSE)
@@ -50,3 +85,15 @@ pdf(plot_f)
 hist(m$delta, breaks=100, main='PDO-PDX delta ggistic scores')
 #lines(density(m$delta))
 graphics.off()
+
+
+
+gglm <- function(x, y, nx, ny, title, file) {
+  pe <- cor.test(x, y)
+  d <- data.frame(x=x, y=y)
+  ggplot(d, aes(x=x, y=y)) +geom_point(size=2)+current_theme+xlab(nx)+ylab(ny)+labs(caption=paste0(round(pe$estimate, digits=3), ', pval=', formatC(pe$p.value, format = "e", digits = 3)))+ggtitle(title)+geom_smooth(method=lm)+current_theme
+  ggsave(file, width=10, height=10, units="in")
+}
+
+
+gglm(m$pdo_gistic_score, m$pdx_gistic_score, 'pdo_ggistic','pdx_ggistic','Gene Gistic Scores', plot2_f)
