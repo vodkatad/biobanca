d4f862442e6d6d2c1e1e691b011a46084da078e0
Elena Grassi <grassi.e@gmail.com>
2021-09-29 17:18:51 +0200

    Heatmap with colors different/same mut for x/o

--- local/src/wip_targeted.R
+++ local/src/wip_targeted.R
@@ -518,7 +518,11 @@ colnames(td2x) <- substr(colnames(td2x), 0, 7)
 colnames(td2o) <- substr(colnames(td2o), 0, 7)
 
 td2x <- td2x[, colnames(td2x) != "CRC1875"]
-mat_list <- list(OSNV=td2o, XSNV=td2x)
+#mat_list <- list(OSNV=td2o, XSNV=td2x)
+both <- td2o & td2x
+both2 <- t(apply(both, 1, as.numeric))
+rownames(both2) <- rownames(both)
+mat_list <- list(Both=both, PDO= td2o-both, PDX=td2x-both  ) 
 
 #rownames(d3pdo) <- rownames(d2pdo)
 #colnames(d3pdo) <- colnames(d2pdo)
@@ -535,35 +539,35 @@ mat_list <- list(OSNV=td2o, XSNV=td2x)
 
 #d3 <- apply(d2, 2, function(x) { f <- as.factor(x); levels(f) <- c("","SNV") })
 #col = c("HOMDEL" = "blue", "AMP" = "red", "GAIN"= "#e86f0a", "MISSENSE" = "#008000", "STOP" = "#91f903", "INDEL" = "#01c601", "OTHER"="#9a4af9")
-col = c("HOMDEL" = "blue", "AMP" = "red", "GAIN"= "#e86f0a", "XSNV" = "#ffcc00", "OTHER"="#9a4af9", "OSNV" = "#008000")
+col = c("Both" = "blue", "PDO" = "red", "PDX"= "#0b7015")#, "XSNV" = "#ffcc00", "OTHER"="#9a4af9", "OSNV" = "#008000")
 alter_fun = list(
-  #background = function(x, y, w, h) {
-  #  grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), 
-  #            gp = gpar(fill = "#CCCCCC", col = NA))
-  #},
   background = function(x, y, w, h) {
-    grid.polygon(
-      unit.c(x - 0.5*w, x - 0.5*w, x + 0.5*w), 
-      unit.c(y - 0.5*h, y + 0.5*h, y - 0.5*h),
-      gp = gpar(fill = "grey", col = "white"))
-    grid.polygon(
-      unit.c(x + 0.5*w, x + 0.5*w, x - 0.5*w), 
-      unit.c(y + 0.5*h, y - 0.5*h, y + 0.5*h),
-      gp = gpar(fill = "grey", col = "white"))
-  },
-  # big blue
-  HOMDEL = function(x, y, w, h) {
     grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), 
-              gp = gpar(fill = col["HOMDEL"], col = NA))
+              gp = gpar(fill = "#CCCCCC", col = NA))
+  },
+  #background = function(x, y, w, h) {
+  #  grid.polygon(
+  #    unit.c(x - 0.5*w, x - 0.5*w, x + 0.5*w), 
+  #    unit.c(y - 0.5*h, y + 0.5*h, y - 0.5*h),
+  #    gp = gpar(fill = "grey", col = "white"))
+  #  grid.polygon(
+  #    unit.c(x + 0.5*w, x + 0.5*w, x - 0.5*w), 
+  #    unit.c(y + 0.5*h, y - 0.5*h, y + 0.5*h),
+  #    gp = gpar(fill = "grey", col = "white"))
+  #},
+  # big blue
+  Both = function(x, y, w, h) {
+    grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), 
+              gp = gpar(fill = col["Both"], col = NA))
   },
   # bug red
-  AMP = function(x, y, w, h) {
+  PDO = function(x, y, w, h) {
     grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), 
-              gp = gpar(fill = col["AMP"], col = NA))
+              gp = gpar(fill = col["PDO"], col = NA))
   },
-  GAIN = function(x, y, w, h) {
+  PDX = function(x, y, w, h) {
     grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), 
-              gp = gpar(fill = col["GAIN"], col = NA))
+              gp = gpar(fill = col["PDX"], col = NA))
   },
   # small green
   # MISSENSE = function(x, y, w, h) {
@@ -578,19 +582,19 @@ alter_fun = list(
   #   grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, 
   #             gp = gpar(fill = col["INDEL"], col = NA))
   # },
-  OSNV = function(x, y, w, h) {
-    grid.polygon(
-      unit.c(x - 0.5*w, x - 0.5*w, x + 0.5*w), 
-      unit.c(y - 0.5*h, y + 0.5*h, y - 0.5*h),
-      gp = gpar(fill = col["OSNV"], col = "white"))
-    
-  },
-  XSNV = function(x, y, w, h) {
-    grid.polygon(
-      unit.c(x + 0.5*w, x + 0.5*w, x - 0.5*w), 
-      unit.c(y + 0.5*h, y - 0.5*h, y + 0.5*h),
-      gp = gpar(fill = col["XSNV"], col = "white"))
-  },
+  # OSNV = function(x, y, w, h) {
+  #   grid.polygon(
+  #     unit.c(x - 0.5*w, x - 0.5*w, x + 0.5*w), 
+  #     unit.c(y - 0.5*h, y + 0.5*h, y - 0.5*h),
+  #     gp = gpar(fill = col["OSNV"], col = "white"))
+  #   
+  # },
+  # XSNV = function(x, y, w, h) {
+  #   grid.polygon(
+  #     unit.c(x + 0.5*w, x + 0.5*w, x - 0.5*w), 
+  #     unit.c(y + 0.5*h, y - 0.5*h, y + 0.5*h),
+  #     gp = gpar(fill = col["XSNV"], col = "white"))
+  # },
   OTHER = function(x, y, w, h) {
     grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, 
               gp = gpar(fill = col["OTHER"], col = NA))
@@ -648,9 +652,26 @@ oncoPrint(td4,
 mat_list2 <- list()
 mat_list2[[1]] <- mat_list[[1]][,seq(1,30)]
 mat_list2[[2]] <- mat_list[[2]][,seq(1,30)]
+mat_list2[[3]] <- mat_list[[3]][,seq(1,30)]
 names(mat_list2) <- names(mat_list)
+
+m <- mat_list[[1]]
+
+d <- read.table('/mnt/trcanmed/snaketree/prj/pdxopedia/local/share/data/treats/august2020/Treatments_Eugy_Ele_fix0cetuxi_201005_cetuxi3w_CRC0078_PR.tsv', sep="\t", header=FALSE, stringsAsFactors = TRUE, row.names=1)
+
+length(intersect(rownames(d), colnames(m)))
+
+s <- mat_list2[[1]]+mat_list2[[2]]+mat_list2[[3]]
+su <- colSums(s)
+su <- su[order(-su)]
+
+oncoPrint(mat_list2, alter_fun = alter_fun, col = col)
+oncoPrint(mat_list2, alter_fun = alter_fun, col = col, column_order = names(su))
+
 oncoPrint(mat_list2,
-          alter_fun = alter_fun, col = col)
+          alter_fun = alter_fun, col = col, top_annotation = HeatmapAnnotation(cbar = anno_oncoprint_barplot(),
+                                                                               Cetuximab = anno_barplot(dd$perc, gp = gpar(fill = get_recist(dd$perc))), height = unit(4, "cm")),
+          row_names_gp = gpar(fontsize=8))
 
 pp <- oncoPrint(mat_list, alter_fun = alter_fun, col = col);
 
