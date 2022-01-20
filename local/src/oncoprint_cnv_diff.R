c65bdb50f7db8af65c0d954b59e996908ed51945
Elena Grassi <grassi.e@gmail.com>
2022-01-20 23:51:27 +0100

    [shallow] oncoprint for ggistic

--- local/src/oncoprint_cnv_diff.R
+++ local/src/oncoprint_cnv_diff.R
@@ -4,18 +4,56 @@ library(ggplot2)
 op_f <- snakemake@output[['op']]
 op_data_f <- snakemake@output[['op_data']]
 pie_f <- snakemake@output[['pie']]
+thr <- as.numeric(snakemake@wildcards[['top']])
+pdo_f <- snakemake@input[['pdo']]
+xeno_f <- snakemake@input[['xeno']]
+
 load(snakemake@input[['Rimage']])
 #eval(parse(text=myriad))
 
-mergemut <- merge(pdobing, pdxbing, by="row.names")#, all.x=TRUE, all.y=TRUE, fill=FALSE) # no need all the same genes:
-#> setdiff(rownames(pdo_df), rownames(xeno_df))
-#character(0)
-# TODO TRUE?
+
+pdobing <- read.table(pdo_f, sep="\t", row.names=1, stringsAsFactors=FALSE, header=TRUE)
+pdxbing <- read.table(xeno_f, sep="\t", row.names=1, stringsAsFactors=FALSE, header=TRUE)
+pdobing[,1] <- NULL
+pdobing[,1] <- NULL
+pdxbing[,1] <- NULL
+pdxbing[,1] <- NULL
+
+
+pdobing <- pdobing[, colnames(pdobing) != "CRC0177LMO0D04017001D02000"] # remove the mutated pdo
+pdxbing <- pdxbing[,colnames(pdxbing) != "CRC0177LMX0B05001TUMD06000"] # remove the wt xeno
+
+list_remove_xeno <- c("CRC1870", "CRC1875","CRC2041")
+list_lmh <- colnames(pdobing)[grepl('LMH', colnames(pdobing))]
+model_lmh <- substr(list_lmh, 0,7)
+
+pdobing <- pdobing[, !colnames(pdobing) %in% list_lmh]
+pdobing <- pdobing[, !substr(colnames(pdobing),0,7) %in% model_lmh]
+pdxbing <- pdxbing[, !substr(colnames(pdxbing),0,7) %in% model_lmh]
+
+#> setdiff(models_xeno, models_pdo)
+#[1] "CRC1870" "CRC1875" "CRC2041"
+# We know we were missing some PDOs TODO load from file if qc changes in the future
+pdxbing <- pdxbing[, !substr(colnames(pdxbing),0,7) %in% list_remove_xeno]
+
+colnames(pdobing) <- substr(colnames(pdobing), 0, 7)
+colnames(pdxbing) <- substr(colnames(pdxbing), 0, 7)
+
+mergemut <- merge(pdobing, pdxbing, by="row.names") ## TODO CHECK THEY HAVE ALL SAME GENES TODO
 rownames(mergemut) <- mergemut$Row.names
 mergemut$Row.names <- NULL
+save.image('pippo.Rdata')
+mergemut[mergemut==-1] <- 0
+nampl <- apply(mergemut, 1, sum)
+nampl <- nampl[order(-nampl)]
+top <- head(nampl, n=thr)
+#mergemut <- mergemut[names(top),]
+mergemut <- mergemut[c('ERBB2','EGFR','MET','MYC', 'IGF2','FGFR1','FGFR2','IRS2','PTEN'),]
+#> setdiff(rownames(pdo_df), rownames(xeno_df))
+#character(0)
 
 d2 <- t(mergemut)
-d2 <- ifelse(d2, 1, 0)
+#d2 <- ifelse(d2, 1, 0)
 #d <- read.table(treat_f, sep="\t", header=FALSE, stringsAsFactors = TRUE, row.names=1)
 
 #rimuovere i non mutati
