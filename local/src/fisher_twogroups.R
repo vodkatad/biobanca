cfaf0927900031a7c21b689a5e20a502dfd53b25
Martina Ferri <martina.ferri@unito.it>
2022-02-25 18:40:14 +0100

    enrichment

---
+++ local/src/fisher_twogroups.R
@@ -0,0 +1,63 @@
+library(reshape)
+
+groups_f <- snakemake@input[['groups']]
+mutmat_f <- snakemake@input[['mutmat']]
+res_f <- snakemake@output[['fisher']]
+log_f <- snakemake@log[['log']]
+outfile <- snakemake@output[['fisher']]
+
+
+save.image('pippo.Rdata')
+
+muts <- read.table(mutmat_f, sep="\t", quote="", header=TRUE, stringsAsFactors = FALSE, row.names = 1)
+muts <- ifelse(muts=="True", TRUE, FALSE)
+
+groups <- read.table(groups_f, sep="\t", quote="", header=TRUE, stringsAsFactors = FALSE)
+groups$group <- ifelse(groups$buoni == "TRUE", TRUE, FALSE)
+
+common <- intersect(colnames(muts), groups$smodel)
+
+sink(log_f)
+print('With muts:')
+ncol(muts)
+print('With groups:')
+nrow(groups)
+print('With both:')
+length(common)
+sink()
+
+muts <- muts[, common, drop=FALSE]
+groups <- groups[groups$smodel %in% common, , drop=FALSE]
+groups <- groups[match(common, groups$smodel), ,drop=FALSE]
+
+sink(log_f, append=TRUE)
+print('Comparing: ')
+table(groups$group)
+sink()
+
+if (! all(groups$smodel == colnames(muts))) {
+  stop('Something fishy in matching mutation info with groups...')
+}
+                 
+fisher_one_gene <- function(gene_tf, group_tf) {
+  n_group <- sum(group_tf)
+  n_not_group <- sum(!group_tf)
+  n_mutated_in_group <- sum(gene_tf[group_tf])
+  n_mutated_not_in_group <- sum(gene_tf[!group_tf])
+  frac_mut_group <- n_mutated_in_group / n_group
+  frac_mut_out_group <- n_mutated_not_in_group / n_not_group
+  fi <- fisher.test(gene_tf, group_tf)
+  return(c(fi$p.value, frac_mut_group, frac_mut_out_group, n_mutated_in_group, n_mutated_not_in_group, n_group, n_not_group))
+}
+
+at_least_one_muts <- muts[apply(muts, 1, function(x) any(x)),]
+all_fisher_fracs <- apply(at_least_one_muts, 1, fisher_one_gene, groups$group)
+
+res <- as.data.frame(t(all_fisher_fracs))
+colnames(res) <- c('pvalue', 'frac_muts_group', 'frac_muts_outgroup', 'n_muts_group', 'n_muts_outgroup', 'n_group', 'n_outgroup')
+res$p.adjust <- p.adjust(res$pvalue, method="BH")
+res <- res[order(res$pvalue),]
+
+res$gene <- rownames(res)
+res <- res[, c(ncol(res), seq(1, (ncol(res)-1)))]
+write.table(res, file=outfile, sep="\t", quote=FALSE, row.names=FALSE)
---
+++ local/src/fra_mut_annotation.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+fra <- snakemake@input[["mut"]]
+res <- snakemake@output[["results"]]
+
+#fra <- "/scratch/trcanmed/pdxopedia/local/share/data/dtb_mutations_vlookupAndrea_expandedPRLM.tsv"
+fra <- read.table(fra, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+fra2 <- fra[!(fra$X=="PR"),]
+fra2$X <- NULL          
+fra2$GenealogyID <- NULL
+fra2$flag_H.X <- NULL
+
+fra3 <- fra2
+cases <- fra3$CASE              
+
+for (i in seq(length(cases))) {
+  fra3$KRAS2 <- ifelse(fra3$KRAS == "wt", "False", "True")
+  fra3$NRAS2 <- ifelse(fra3$NRAS == "wt", "False", "True")
+  fra3$BRAF2 <- ifelse(fra3$BRAF == "wt", "False", "True")
+  fra3$PIK3CA2 <- ifelse(fra3$PIK3CA == "wt", "False", "True")
+}
+
+fra3$KRAS <- NULL
+fra3$NRAS <- NULL
+fra3$BRAF <- NULL
+fra3$PIK3CA <- NULL
+colnames(fra3) <- c('smodel', 'KRAS', 'NRAS', 'BRAF', 'PIK3CA')
+rownames(fra3) <- fra3$smodel
+fra3$smodel <- NULL
+tfra <- as.data.frame(t(fra3))
+tfra <- tibble::rownames_to_column(tfra, "genes")
+
+write.table(tfra, res, sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
