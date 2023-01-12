c51fc3cdebf4dda110a7a2ec3ee296d87a7b5f42
Elena Grassi <grassi.e@gmail.com>
2023-01-12 15:42:46 +0100

    Second y axis (rescaled or not)

--- local/src/EGFR_cetuxi_cor.R
+++ local/src/EGFR_cetuxi_cor.R
@@ -55,7 +55,14 @@ ggsave(out_plot_f, plot=gplot_nol, height=2.5, width=2.5, units='in')
 
 merged_annot$sort <- merged_annot[,col_cetuxi]
 
-maxi <- max(merged_annot$ko_score)
+#https://stackoverflow.com/questions/3099219/ggplot-with-2-y-axes-on-each-side-and-different-scales
+#https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
+ylim.prim <- c(min(merged_annot$ko_score), max(merged_annot$ko_score))   # in this example, precipitation
+ylim.sec <- c(min(merged_annot$sort), max(merged_annot$sort))    # in this example, temperature
+
+#The following makes the necessary calculations based on these limits, and makes the plot itself:
+b <- diff(ylim.prim)/diff(ylim.sec)
+a <- ylim.prim[1] - b*ylim.sec[1]
 
 gplot <- ggplot(data=merged_annot, aes(x=reorder(smodel, sort), y=ko_score, fill=alterations))+geom_col()+
   unmute_theme +
@@ -63,9 +70,13 @@ gplot <- ggplot(data=merged_annot, aes(x=reorder(smodel, sort), y=ko_score, fill
   ylab('EGFR avg ko score') +
   scale_fill_manual(values=c('darkorange', 'darkred', 'grey40')) +
   labs(color="Relevant somatic alterations") +
+  #scale_y_continuous(sec.axis = sec_axis(~ ., name="CTX treated/untreated luminescence ratio")) +
+  scale_y_continuous(sec.axis = sec_axis(~ (. - a)/b, name="CTX treated/untreated luminescence ratio")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  geom_point(aes_string(y=col_cetuxi), size=0.3)
+  #geom_point(aes_string(y=col_cetuxi), size=0.3)
+  geom_point(aes(y=a+sort*b), size=0.3)
 
+save.image('dolore.Rdata')
 ggsave(out_barplot_f, plot=gplot, height=2.5, width=2.5, units='in')
 
 gplot_nol <- gplot + theme(legend.position = "none")
